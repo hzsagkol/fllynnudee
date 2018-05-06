d67b759cf5252795a7df395884d4d4bc0a39100e
hzsagkol <37846356+hzsagkol@users.noreply.github.com>
2018-05-06 20:08:33 +0300

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -79,6 +79,12 @@ client.unload = command => {
     }
   });
 };
+client.on('message', msg => {
+  if (!msg.content.startsWith(prefix)) {
+    console.log(`[${msg.author.tag}]  : ${msg.content}`);
+    return;
+  }
+});
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
